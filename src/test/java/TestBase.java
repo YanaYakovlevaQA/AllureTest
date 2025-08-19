3883e50062a17a7b444a4a047cf1e5a9d1ecdf29
Yana Yakovleva <yanna2013yanna@gmail.com>
2025-08-19 23:02:15 +0500

    add @BeforeAll

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,10 +1,9 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 
-import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
-    @BeforeEach
+    @BeforeAll
      void setUp() {
         Configuration.browserSize = "1920x1980";
         Configuration.pageLoadStrategy = "eager";
